e3aab73a053607c1014230f31b263a34aee41b2d
UllrAthletics <46335896+UllrAthletics@users.noreply.github.com>
2019-01-14 21:32:57 -0700

    Delete SQL_creatingTables.sql

--- SQL_creatingTables.sql
+++
@@ -1,9 +0,0 @@
-
-
-create table Surgery( --create table statement is not case sensitive
-	SurgeryNo int,
-	SurgeryName varchar(50), -- in sql you must specify num of characters when setting type
-	SurgeryType varchar(50),
-	Constraint pk_Surgery primary key(SurgeryNo)
-);
-GO -- go statement terminates batch
